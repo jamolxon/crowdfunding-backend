e37d6ac34b523bd17be96b8e0c092842e74e68b5
Jamolkhon <jamolxonahmedov124@gmail.com>
2024-09-04 02:18:20 +0500

    initial commit

---
+++ core/local_settings.example.py
@@ -0,0 +1,53 @@
+# Configuration of project in settings.py is overridden here
+
+ALLOWED_HOSTS = ["*"]
+DEBUG = True
+
+
+# Database configuration
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "lenta",
+        "USER": "postgres",
+        "PASSWORD": "admin",
+        "HOST": "localhost",
+        "PORT": "5432",
+        "ATOMIC_REQUESTS": True,
+    }
+}
+
+# Host configuration
+
+HOST = "http://localhost:8000"
+
+FRONT_REDIRECT_URL = "https://lenta.uicgroup.tech/auth/verify/"
+MODERATION_HOST = "https://lentabeta.xn--h28h.uz"
+
+# CACHES = {
+#     "default": {
+#         "BACKEND": "django_redis.cache.RedisCache",
+#         "LOCATION": "redis://127.0.0.1:6379/1",
+#         "OPTIONS": {"CLIENT_CLASS": "django_redis.client.DefaultClient"},
+#         "KEY_PREFIX": "lenta",
+#     },
+#     "qr-code": {
+#         "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+#         "LOCATION": "qr-code-cache",
+#         "TIMEOUT": 3600,
+#     },
+# }
+# SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+
+
+# # CELERY
+# BROKER_URL = "amqp://localhost"
+# CELERY_RESULT_BACKEND = "rpc://"
+# CELERY_ACCEPT_CONTENT = ["application/json"]
+# CELERY_TASK_SERIALIZER = "json"
+# CELERY_RESULT_SERIALIZER = "json"
+# CELERY_AMQP_TASK_RESULT_EXPIRES = 1000
+# CELERY_TIMEZONE = "Asia/Tashkent"
+
+
